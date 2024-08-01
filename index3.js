4e36774151ba1e3b3b1f42bacc349706def9e988
anyoneout <chrisdafur@gmail.com>
2024-08-01 18:04:00 -0500

    added return function refrain to song

--- index3.js
+++ index3.js
@@ -1,5 +1,5 @@
 
-function refrain () {
+function refrain() {
   let refrainLyrics = "";
   refrainLyrics += "I'm talkin' 'bout Atom, and Evil\n"
   refrainLyrics += "Atom and Evil\n"
@@ -8,7 +8,7 @@ function refrain () {
   return refrainLyrics;
 }
 function verseOne() {
-  let  verseOneLyrics = "";
+  let verseOneLyrics = "";
   verseOneLyrics += "Now brothers and sisters\n"
   verseOneLyrics += "I'm troubled to say\n"
   verseOneLyrics += "Brother Atom is gone astray\n"
@@ -24,44 +24,44 @@ function verseTwo() {
   return verseTwoLyrics;
 }
 function verseThree() {
-  let  verseThreeLyrics = "";
-verseThreeLyrics += "Now Atom was an honest, hard workin' man\n"
-verseThreeLyrics += "He wanted to help out the human clan\n"
-verseThreeLyrics += "But Evil got him drunk on prejudice and hate\n"
-verseThreeLyrics += "And she taught him how to gamble with Humanity's fate\n"
-verseThreeLyrics += "(So true!)\n"
-return verseThreeLyrics;
+  let verseThreeLyrics = "";
+  verseThreeLyrics += "Now Atom was an honest, hard workin' man\n"
+  verseThreeLyrics += "He wanted to help out the human clan\n"
+  verseThreeLyrics += "But Evil got him drunk on prejudice and hate\n"
+  verseThreeLyrics += "And she taught him how to gamble with Humanity's fate\n"
+  verseThreeLyrics += "(So true!)\n"
+  return verseThreeLyrics;
 }
 function verseFour() {
-  let  verseFourLyrics = "";
-verseFourLyrics += "Now if Evil gets Atom, 'twill be such a shame\n"
-verseFourLyrics += "Because a-plenty of big shots are playin' that dame\n"
-verseFourLyrics += "Now his sleep will be troubled, and his life will be cursed\n"
-verseFourLyrics += "Lord, if Atom plays with evil, Jack, he won't be the first!\n"
-verseFourLyrics += "(So true!)\n\n"
-return verseFourLyrics;
+  let verseFourLyrics = "";
+  verseFourLyrics += "Now if Evil gets Atom, 'twill be such a shame\n"
+  verseFourLyrics += "Because a-plenty of big shots are playin' that dame\n"
+  verseFourLyrics += "Now his sleep will be troubled, and his life will be cursed\n"
+  verseFourLyrics += "Lord, if Atom plays with evil, Jack, he won't be the first!\n"
+  verseFourLyrics += "(So true!)\n\n"
+  return verseFourLyrics;
 }
 function verseFive() {
-  let  verseFiveLyrics = "";
-verseFiveLyrics += "Now Atom is a youngster and pretty hard to handle\n"
-verseFiveLyrics += "But we better step in and stop that scandal\n"
-verseFiveLyrics += "Because if Atom and Evil should ever be wed\n"
-verseFiveLyrics += "Lord, then darn near all of us are goin' to be dead!\n"
-verseFiveLyrics += "(So True)\n\n"
-return verseFiveLyrics;
+  let verseFiveLyrics = "";
+  verseFiveLyrics += "Now Atom is a youngster and pretty hard to handle\n"
+  verseFiveLyrics += "But we better step in and stop that scandal\n"
+  verseFiveLyrics += "Because if Atom and Evil should ever be wed\n"
+  verseFiveLyrics += "Lord, then darn near all of us are goin' to be dead!\n"
+  verseFiveLyrics += "(So True)\n\n"
+  return verseFiveLyrics;
 }
 function verseSix() {
-  let  verseSixLyrics = "";
-verseSixLyrics += "Atom and Evil\n"
-verseSixLyrics += "Atom and Evil\n"
-verseSixLyrics += "If you don't break up that romance soon\n"
-verseSixLyrics += "We'll all fall down and go boom, boom, (boom), boom!\n"
-verseSixLyrics += "We're sitting on the edge of doom (doom) (doom) (doom) doom!\n"
-return verseSixLyrics;
+  let verseSixLyrics = "";
+  verseSixLyrics += "Atom and Evil\n"
+  verseSixLyrics += "Atom and Evil\n"
+  verseSixLyrics += "If you don't break up that romance soon\n"
+  verseSixLyrics += "We'll all fall down and go boom, boom, (boom), boom!\n"
+  verseSixLyrics += "We're sitting on the edge of doom (doom) (doom) (doom) doom!\n"
+  return verseSixLyrics;
 }
 
 
-function song(){
+function song() {
   console.log(verseOne());
   console.log(verseTwo());
   console.log(verseThree());
