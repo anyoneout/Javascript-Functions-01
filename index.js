c9f534fca000db42d3461c030730b7b4bd8cd4a7
anyoneout <chrisdafur@gmail.com>
2024-08-01 17:21:57 -0500

    extracted repetitions into functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+function imTalkinBoutAtom(){
+  console.log("I'm talkin' 'bout Atom, and Evil");
+  console.log("Atom and Evil");
+  console.log("If you don't break up that romance soon");
+  console.log("We'll all fall down and go boom, boom, boom!");
+}
 
 function song(){
 console.log("Now brothers and sisters");
@@ -14,28 +19,19 @@ console.log("He wanted to help out the human clan");
 console.log("But Evil got him drunk on prejudice and hate");
 console.log("And she taught him how to gamble with Humanity's fate");
 console.log("(So true!)")
-console.log("I'm talkin' 'bout Atom, and Evil");
-console.log("Atom and Evil");
-console.log("If you don't break up that romance soon");
-console.log("We'll all fall down and go boom, boom, boom!");
+imTalkinBoutAtom();
 console.log("Now if Evil gets Atom, 'twill be such a shame");
 console.log("Because a-plenty of big shots are playin' that dame");
 console.log("Now his sleep will be troubled, and his life will be cursed");
 console.log("Lord, if Atom plays with evil, Jack, he won't be the first!");
 console.log("(So true!");
-console.log("I'm talkin' 'bout Atom, and Evil");
-console.log("Atom and Evil");
-console.log("If you don't break up that romance soon");
-console.log("We'll all fall down and go boom, boom, boom!");
+imTalkinBoutAtom();
 console.log("Now Atom is a youngster and pretty hard to handle");
 console.log("But we better step in and stop that scandal");
 console.log("Because if Atom and Evil should ever be wed");
 console.log("Lord, then darn near all of us are goin' to be dead!");
 console.log("(So True)");
-console.log("I'm talkin' 'bout Atom, and Evil");
-console.log("Atom and Evil");
-console.log("If you don't break up that romance soon");
-console.log("We'll all fall down and go boom, boom, boom!");
+imTalkinBoutAtom();
 console.log("Atom and Evil");
 console.log("Atom and Evil");
 console.log("If you don't break up that romance soon");
