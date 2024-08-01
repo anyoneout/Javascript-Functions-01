627286d2595838c551f33413310eaf8bfe859dfd
anyoneout <chrisdafur@gmail.com>
2024-08-01 17:38:59 -0500

    added return function refrain to song

---
+++ index2.js
@@ -0,0 +1,46 @@
+
+function refrain () {
+  let refrainLyrics = "";
+  refrainLyrics += "I'm talkin' 'bout Atom, and Evil\n"
+  refrainLyrics += "Atom and Evil\n"
+  refrainLyrics += "If you don't break up that romance soon\n"
+  refrainLyrics += "We'll all fall down and go boom, boom, boom!\n"
+  return refrainLyrics;
+}
+function song(){
+  console.log("Now brothers and sisters");
+  console.log("I'm troubled to say");
+  console.log("Brother Atom is gone astray");
+  console.log("Listen, listen, listen, listen, listen");
+  console.log("This is the story of Atom and Evil");
+  console.log("Their courtship is causin' a great upheaval");
+  console.log("Now Atom was a sweet young innocent thing");
+  console.log("Until the night that Miss Evil took him under her wing");
+  console.log("Now Atom was an honest, hard workin' man");
+  console.log("He wanted to help out the human clan");
+  console.log("But Evil got him drunk on prejudice and hate");
+  console.log("And she taught him how to gamble with Humanity's fate");
+  console.log("(So true!)")
+  console.log(refrain());
+  console.log("Now if Evil gets Atom, 'twill be such a shame");
+  console.log("Because a-plenty of big shots are playin' that dame");
+  console.log("Now his sleep will be troubled, and his life will be cursed");
+  console.log("Lord, if Atom plays with evil, Jack, he won't be the first!");
+  console.log("(So true!");
+  console.log(refrain());
+  console.log("Now Atom is a youngster and pretty hard to handle");
+  console.log("But we better step in and stop that scandal");
+  console.log("Because if Atom and Evil should ever be wed");
+  console.log("Lord, then darn near all of us are goin' to be dead!");
+  console.log("(So True)");
+  console.log(refrain());
+  console.log("Atom and Evil");
+  console.log("Atom and Evil");
+  console.log("If you don't break up that romance soon");
+  console.log("We'll all fall down and go boom, boom, (boom), boom!");
+  console.log("We're sitting on the edge of doom (doom) (doom) (doom) doom!");
+  }
+  
+  console.log(song());
+
+
